5cdc9b0e5745e31abba8a51a0a94358237a83c51
herbertliu <herbertliuhb@gmail.com>
2015-02-06 14:28:29 +0800

    node_modules...

--- node_modules/lego-client/lib/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.config = require('./config');
-exports.publish = require('./publish');
-exports.unpublish = require('./unpublish');
-exports.login = require('./login');
-exports.install = require('./install');
-exports.info = require('./info');
-exports.search = require('./search');
